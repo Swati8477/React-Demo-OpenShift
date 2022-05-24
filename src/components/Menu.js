df1d2031148af4bd97396f10f45babafb63ce92a
Swati8477 <swatinegi406@gmail.com>
2022-05-24 14:36:50 +0530

    adding new files

---
+++ src/components/Menu.js
@@ -0,0 +1,75 @@
+const Menu = [
+    {
+        id: 1,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 2,
+        image: "",
+        name: "Chewmine",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 3,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 4,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 5,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 6,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 7,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+
+    {
+        id: 8,
+        image: "",
+        name: "maggie",
+        category: "breakfast",
+        price: "12₹",
+        description: "The Maggi brand originates from Switzerland where in 1886 Julius Maggi created a recipe of flavours to bring added taste to meals."
+    },
+];
+
+export default Menu;
