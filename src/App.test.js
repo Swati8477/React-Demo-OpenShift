1cd5e2cfa5936bb563e63de281ac3c24e42069fc
Swati8477 <swatinegi406@gmail.com>
2022-05-29 13:12:12 +0530

    adding test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<App />, div);
+  });
+  
 it('renders welcome message', () => {
   render(<App />);
   expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
+
