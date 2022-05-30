bcc793c79a0a1aa2c6566b4295b3b3b612553640
Swati8477 <swatinegi406@gmail.com>
2022-05-30 14:44:22 +0530

    adding

--- src/App.js
+++ src/App.js
@@ -8,18 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello welcome to the React !
-        </a>
+        Hello welcome to the React JS
       </header>
     </div>
   );
